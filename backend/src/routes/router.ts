addf8035d4cab16de0a41a8f581b37811c357fab
Ivanzmartins <ivanzmartins@gmail.com>
2024-10-27 16:52:42 -0300

    .

--- backend/src/routes/router.ts
+++ backend/src/routes/router.ts
@@ -1,12 +1,9 @@
 import { Router } from 'express';
-import energyInvoiceRouter from './energy.router';
 
 const router = Router();
 
 router.get('/', async (_req, res) => {
-	res.send('Hello, world!');
+  res.send('Hello, world!');
 });
-
-router.use('/energy', energyInvoiceRouter);
 
 export default router;
